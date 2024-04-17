cfa522a77257084497b6100fb52297aa1c0879fc
Anastasiia Smirnova <nyakubtsova@gmail.com>
2024-04-17 15:07:48 +0200

    fix tests

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 [![Actions Status](https://github.com/AMSmirnova/java-project-99/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/AMSmirnova/java-project-99/actions)
 
 [![Java-CI](https://github.com/AMSmirnova/java-project-99/actions/workflows/main.yml/badge.svg)](https://github.com/AMSmirnova/java-project-99/actions/workflows/main.yml)
-[![Maintainability](https://api.codeclimate.com/v1/badges/214828ab3400c1100cf3/maintainability)](https://codeclimate.com/github/AMSmirnova/java-project-99/maintainability)
-[![Test Coverage](https://api.codeclimate.com/v1/badges/214828ab3400c1100cf3/test_coverage)](https://codeclimate.com/github/AMSmirnova/java-project-99/test_coverage)
-
+[![Maintainability](https://api.codeclimate.com/v1/badges/0ba13c6552516d95fa11/maintainability)](https://codeclimate.com/github/AMSmirnova/java-project-99/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/0ba13c6552516d95fa11/test_coverage)](https://codeclimate.com/github/AMSmirnova/java-project-99/test_coverage)
 # Task manager
 
 
--- src/main/java/hexlet/code/controller/AuthenticationController.java
+++ src/main/java/hexlet/code/controller/AuthenticationController.java
@@ -3,12 +3,10 @@ package hexlet.code.controller;
 import hexlet.code.dto.AuthRequest;
 import hexlet.code.util.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -20,6 +18,7 @@ public class AuthenticationController {
     private AuthenticationManager authenticationManager;
 
     @PostMapping(path = "/login")
+    @ResponseStatus(HttpStatus.OK)
     public String create(@RequestBody AuthRequest authRequest) {
         var authentication = new UsernamePasswordAuthenticationToken(
                 authRequest.getUsername(), authRequest.getPassword());
