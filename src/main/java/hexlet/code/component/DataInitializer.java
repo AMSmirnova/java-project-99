ec0445152ea125768920b6a86b113abf9e2c7bc2
Anastasiia Smirnova <nyakubtsova@gmail.com>
2024-04-19 13:35:38 +0200

    fix render

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -7,7 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.Profile;
+//import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
