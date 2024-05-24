a025553ed93317b582afac3cb4abecf8b9d1462a
Anastasiia Smirnova <nyakubtsova@gmail.com>
2024-05-24 18:32:51 +0200

    fix Missing node in path taskLabelIds

--- src/main/java/hexlet/code/mapper/TaskMapper.java
+++ src/main/java/hexlet/code/mapper/TaskMapper.java
@@ -3,13 +3,20 @@ package hexlet.code.mapper;
 import hexlet.code.dto.task.TaskCreateDTO;
 import hexlet.code.dto.task.TaskDTO;
 import hexlet.code.dto.task.TaskUpdateDTO;
+import hexlet.code.model.Label;
 import hexlet.code.model.Task;
+import hexlet.code.repository.LabelRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Set;
 
 
 @Mapper(
@@ -19,6 +26,9 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class TaskMapper {
+
+    @Autowired
+    private LabelRepository labelRepository;
 
     @Mapping(source = "title", target = "name")
     @Mapping(source = "content", target = "description")
@@ -30,8 +40,8 @@ public abstract class TaskMapper {
     @Mapping(source = "description", target = "content")
     @Mapping(source = "name", target = "title")
     @Mapping(source = "taskStatus.slug", target = "status")
-    @Mapping(target = "assigneeId", ignore = true)
-    @Mapping(target = "taskLabelIds", ignore = true)
+    @Mapping(source = "assignee.id", target = "assigneeId")
+    @Mapping(source = "labels", target = "taskLabelIds", qualifiedByName = "toIds")
     public abstract TaskDTO map(Task task);
 
     @Mapping(source = "title", target = "name")
@@ -41,4 +51,10 @@ public abstract class TaskMapper {
     @Mapping(target = "labels", ignore = true)
     public abstract void update(TaskUpdateDTO data, @MappingTarget Task model);
 
+    @Named("toIds")
+    public List<Long> toIds(Set<Label> labels) {
+        return labels == null ? null : labels.stream()
+                .map(Label::getId)
+                .toList();
+    }
 }
