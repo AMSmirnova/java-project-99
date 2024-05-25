742d16f70461a837c6a65a4bb521e4e8ddbfccf9
Anastasiia Smirnova <nyakubtsova@gmail.com>
2024-05-25 12:46:46 +0200

    fix email validation

--- src/main/java/hexlet/code/dto/user/UserCreateDTO.java
+++ src/main/java/hexlet/code/dto/user/UserCreateDTO.java
@@ -2,6 +2,7 @@ package hexlet.code.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,6 +16,7 @@ public class UserCreateDTO {
     private String lastName;
 
     @Email
+    @Pattern(regexp = "[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\\.[a-zA-Z0-9_-]+")
     private String email;
 
     @NotNull
